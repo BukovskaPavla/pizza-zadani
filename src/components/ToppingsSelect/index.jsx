d9afa00d74dacf2945969c22b5d01f4fcb82fe1a
BukovskaPavla <bukovskapavla@gmail.com>
2022-10-25 12:42:32 +0200

    state lifting - funkcni scitani polozek a celkova cena

--- src/components/ToppingsSelect/index.jsx
+++ src/components/ToppingsSelect/index.jsx
@@ -1,16 +1,40 @@
 import React, { useState } from "react";
 import Topping from "../Topping";
-import './style.css';
+import "./style.css";
 
 const ToppingsSelect = ({ toppings }) => {
+  const [checked, setChecked] = useState(toppings);
+
+  const handleChecked = (index, newChecked) => {
+    setChecked((prevValue) => {
+      const checkedToppings = [...prevValue];
+      checkedToppings[index].selected = newChecked;
+      return checkedToppings;
+    });
+  };
+
+  let checkedCount = 0
+  checked.forEach((topping) => checkedCount += topping.selected)
+  
+  let totalPrice = 0
+  checked.forEach((topping) => {if(topping.selected){
+        totalPrice += topping.price 
+      } else {}}) 
+  
+
   return (
     <>
       <p>Choose as many toppings as you want</p>
-      <p>Selected toppings: 0, total price: 0 Euro</p>
-        
-      <div class="toppings">
-        {toppings.map((topping) => (
-          <Topping topping={topping} key={topping.name} />
+      <p>Selected toppings: {checkedCount}, total price: {totalPrice} Euro</p>
+
+      <div className="toppings">
+        {toppings.map((topping, index) => (
+          <Topping
+            topping={topping}
+            key={topping.name}
+            checked={topping.selected}
+            onChange={(newChecked) => handleChecked(index, newChecked)}
+          />
         ))}
       </div>
     </>
